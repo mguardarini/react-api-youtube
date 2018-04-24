7108b610e1969acb6fde0bfd22f556c1a29fe61e
Mauro Guardarini <mauro.guardarini@gmail.com>
2018-04-24 15:46:02 -0300

    update component video detail

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from "youtube-api-search";
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 
 const api_key = "AIzaSyDhN0rdnmMMJuoocA9ysxWe0AzkJnupZvU";
@@ -33,6 +34,7 @@ class App extends Component{
             <div> 
 
                 <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos}/>
 
             </div>
