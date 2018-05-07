6d81c968655dbdabb969f145f6b20bee14cd1ee3
Mauro Guardarini <mauro.guardarini@gmail.com>
2018-05-06 21:03:37 -0300

    create to itens share and like

--- src/components/bottom_navigation.js
+++ src/components/bottom_navigation.js
@@ -3,11 +3,11 @@ import FontIcon from 'material-ui/FontIcon';
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
 import Paper from 'material-ui/Paper';
 import IconLike from 'material-ui/svg-icons/action/favorite-border';
-import IconDeslike from 'material-ui/svg-icons/content/block.js';
+import IconShare from 'material-ui/svg-icons/social/share.js';
 
 const recentsIcon = <FontIcon className="material-icons"></FontIcon>;
-const favoritesIcon = <FontIcon className="material-icons"></FontIcon>;
-const nearbyIcon = <IconLike />;
+const shareIcon = <IconShare/>;
+const likeIcon = <IconLike />;
 
 /**
  * A simple example of `BottomNavigation`, with three labels and icons
@@ -25,19 +25,16 @@ class Bottom extends Component {
     return (
       <Paper zDepth={1}>
         <BottomNavigation selectedIndex={this.state.selectedIndex}>
+          
           <BottomNavigationItem
-            label="Recents"
-            icon={recentsIcon}
+            label="Share"
+            icon={shareIcon}
             onClick={() => this.select(0)}
           />
           <BottomNavigationItem
-            label="Favorites"
-            icon={favoritesIcon}
+            icon={likeIcon}
+            label='Like'
             onClick={() => this.select(1)}
-          />
-          <BottomNavigationItem
-            icon={nearbyIcon}
-            onClick={() => this.select(2)}
           />
         </BottomNavigation>
       </Paper>
